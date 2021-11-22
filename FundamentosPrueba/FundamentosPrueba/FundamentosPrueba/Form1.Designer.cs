838fc79adc5693a67676fdef407dc684e5f788ac
alvaroyako <alvaroyako2000@hotmail.com>
2021-11-22 11:24:20 +0100

    commit

--- FundamentosPrueba/FundamentosPrueba/FundamentosPrueba/Form1.Designer.cs
+++ FundamentosPrueba/FundamentosPrueba/FundamentosPrueba/Form1.Designer.cs
@@ -29,13 +29,73 @@ namespace FundamentosPrueba
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnPulsame = new System.Windows.Forms.Button();
+            this.labelName = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.btnPrueba = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnPulsame
+            // 
+            this.btnPulsame.Location = new System.Drawing.Point(12, 246);
+            this.btnPulsame.Name = "btnPulsame";
+            this.btnPulsame.Size = new System.Drawing.Size(108, 51);
+            this.btnPulsame.TabIndex = 0;
+            this.btnPulsame.Text = "Pulsame";
+            this.btnPulsame.UseVisualStyleBackColor = true;
+            this.btnPulsame.Click += new System.EventHandler(this.btnPulsame_Click);
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Font = new System.Drawing.Font("Comic Sans MS", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelName.Location = new System.Drawing.Point(12, 20);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(129, 56);
+            this.labelName.TabIndex = 1;
+            this.labelName.Text = "Name";
+            this.labelName.Click += new System.EventHandler(this.labelName_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(147, 38);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(137, 23);
+            this.txtName.TabIndex = 2;
+            this.txtName.TextChanged += new System.EventHandler(this.txtName_TextChanged);
+            // 
+            // btnPrueba
+            // 
+            this.btnPrueba.Location = new System.Drawing.Point(264, 246);
+            this.btnPrueba.Name = "btnPrueba";
+            this.btnPrueba.Size = new System.Drawing.Size(111, 51);
+            this.btnPrueba.TabIndex = 3;
+            this.btnPrueba.Text = "Prueba";
+            this.btnPrueba.UseVisualStyleBackColor = true;
+            this.btnPrueba.Click += new System.EventHandler(this.btnPrueba_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(387, 309);
+            this.Controls.Add(this.btnPrueba);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.labelName);
+            this.Controls.Add(this.btnPulsame);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnPulsame;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button btnPrueba;
     }
 }
 
--- FundamentosPrueba/FundamentosPrueba/FundamentosPrueba/Form1.cs
+++ FundamentosPrueba/FundamentosPrueba/FundamentosPrueba/Form1.cs
@@ -17,5 +17,32 @@ namespace FundamentosPrueba
             InitializeComponent();
         }
 
+        private void labelName_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPulsame_Click(object sender, EventArgs e)
+        {
+            this.txtName.Width = 250;
+            this.btnPrueba.Text = "Pulsado...";
+            this.btnPrueba.Location = new Point(20, 50);
+            this.btnPrueba.BackColor = Color.Yellow;
+            this.txtName.TextAlign = HorizontalAlignment.Center;
+
+        }
+
+        private void btnPrueba_Click(object sender, EventArgs e)
+        {
+            int numeroMayor = 999;
+            short numeroMenor = 888;
+            //Conversion automatica
+            numeroMayor = numeroMenor;
+        }
     }
 }
