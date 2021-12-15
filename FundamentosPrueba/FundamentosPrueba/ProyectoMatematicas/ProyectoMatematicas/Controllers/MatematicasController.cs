5824c8a00b7b086951b9de14fb998a9f5234ba8b
alvaroyako <alvaroyako2000@hotmail.com>
2021-12-15 13:58:31 +0100

    commit

--- FundamentosPrueba/FundamentosPrueba/ProyectoMatematicas/ProyectoMatematicas/Controllers/MatematicasController.cs
+++ FundamentosPrueba/FundamentosPrueba/ProyectoMatematicas/ProyectoMatematicas/Controllers/MatematicasController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProyectoMatematicas.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +12,59 @@ namespace ProyectoMatematicas.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult SumarNumeros()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SumarNumeros(int num1, int num2)
+        {
+            int total = num1 + num2;
+            ViewBag.Total = total;
+            return View();
+        }
+
+        public IActionResult Collatz(int numero)
+        {
+            List<int> cadena = new List<int>();
+            
+            while (numero != 1)
+            {
+                if (numero % 2 == 0)
+                {
+                    numero = numero / 2;
+                    cadena.Add(numero);
+                }
+                else
+                {
+                    numero = (numero * 3) + 1;
+                    cadena.Add(numero);
+                }
+
+            }
+            return View(cadena);
+        }
+
+        public IActionResult TablaMultiplicar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult TablaMultiplicar(int num)
+        {
+            List <Multiplicacion> tabla= new List<Multiplicacion>();
+            for(int i = 0; i <= 10; i++)
+            {
+                Multiplicacion mult = new Multiplicacion();
+                mult.Operacion = num + "*" + i;
+                mult.Resultado = num * i;
+                tabla.Add(mult);
+            }
+            return View(tabla);
         }
     }
 }
