a1f645bda1a7074193db254a71a3e7dc78eae7d2
Alvaro Moya Herraiz <alvaro.moya.external@atos.net>
2022-01-08 09:39:11 +0100

    commit

---
+++ ProyectoAlvaroMoyaHerraiz/ProyectoAlvaroMoyaHerraiz/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProyectoAlvaroMoyaHerraiz.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
