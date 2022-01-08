ffca3ca84db155d74b9b860092721bbcec94279a
Alvaro Moya Herraiz <alvaro.moya.external@atos.net>
2022-01-08 16:43:29 +0100

    commit

---
+++ ProyectoAlvaroMoyaHerraiz/ProyectoAlvaroMoyaHerraiz/Models/Plato.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoAlvaroMoyaHerraiz.Models
+{
+    public class Plato
+    {
+        public string Id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public string Categoria { get; set; }
+        public int Precio { get; set; }
+    }
+}
---
+++ ProyectoAlvaroMoyaHerraiz/ProyectoAlvaroMoyaHerraiz/Models/Reserva.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoAlvaroMoyaHerraiz.Models
+{
+    public class Reserva
+    {
+        public string Id { get; set; }
+        public DateTime Fecha { get; set; }
+        public int Asistentes { get; set; }
+        public DateTime Hora { get; set; }
+    }
+}
