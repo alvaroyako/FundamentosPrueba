aca50cf6dace8ee33431225bcc78747a857a8713
alvaroyako <alvaroyako2000@hotmail.com>
2022-02-24 11:33:46 +0100

    commit

---
+++ ServiceWCFLogicaCoches-master/ServiceWCFLogicaCoches/Repositories/RepositoryCoches.cs
@@ -0,0 +1,47 @@
+﻿using ServiceWCFLogicaCoches.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ServiceWCFLogicaCoches.Repositories
+{
+    public class RepositoryCoches
+    {
+        private XDocument document;
+
+        public RepositoryCoches()
+        {
+            //TODO RECURSO INCRUSTADO SE RECUPERA COMO Stream
+            //PARA RECUPERARLO DEBEMOS INDICAR EL NOMBRE DE NUESTRO
+            //namespace Y DE LA CARPETA/S DONDE ESTEN LOS RECURSOS INCRUSTADOS
+            string resourceName =
+                "ServiceWCFLogicaCoches.coches.xml";
+            Stream stream =
+                this.GetType().Assembly
+                .GetManifestResourceStream(resourceName);
+            this.document = XDocument.Load(stream);
+        }
+
+        public List<Coche> GetCoches()
+        {
+            var consulta = from datos in document.Descendants("coche")
+                           select new Coche
+                           {
+                               IdCoche = int.Parse(datos.Element("idcoche").Value),
+                               Marca = datos.Element("marca").Value,
+                               Modelo = datos.Element("modelo").Value,
+                               Imagen = datos.Element("imagen").Value
+                           };
+            return consulta.ToList();
+        }
+
+        public Coche FindCoche(int idcoche)
+        {
+            return this.GetCoches().FirstOrDefault(x => x.IdCoche == idcoche);
+        }
+    }
+}
